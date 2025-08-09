c90a356c5e5a5a3032b38e148dbc5feca35e4064
kauas3228 <kauas3228@gmail.com>
2025-08-08 22:07:59 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,13 +1,10 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
-
 import './global.css';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
-    </>
+    <View className='flex-1 items-center justify-center'>
+      <Text className='text-3xl font-bold'>Hello World</Text>
+    </View>
   );
 }
